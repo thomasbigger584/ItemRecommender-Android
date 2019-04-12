131501d5726954379986ee828ce4e770d17e2eb8
Thomas Bigger <thomasbigger584@gmail.com>
2019-04-12 15:56:29 +0100

    integration of graphql and entrypoint actiuvity for login

---
+++ app/src/main/java/com/twb/itemrecommender/EntrypointActivity.java
@@ -0,0 +1,57 @@
+package com.twb.itemrecommender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
+import com.twb.itemrecommender.product.ProductListActivity;
+
+public class EntrypointActivity extends AppCompatActivity {
+    private final String TAG = EntrypointActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrypoint);
+
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                Log.i(TAG, userStateDetails.getUserState().toString());
+                switch (userStateDetails.getUserState()) {
+                    case SIGNED_IN:
+                        Intent i = new Intent(EntrypointActivity.this, ProductListActivity.class);
+                        startActivity(i);
+                        break;
+                    case SIGNED_OUT:
+                        showSignIn();
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        showSignIn();
+                        break;
+                }
+                finish();
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        });
+    }
+
+    private void showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(this,
+                    SignInUIOptions.builder().nextActivity(ProductListActivity.class).build());
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+}

f1dbf3bb1bde094b6aefbc0f10af73889a81925a
Thomas Bigger <thomas@performanceactive.com>
2019-04-30 14:35:15 +0100

    ability to choose the location and save in shared preferences

--- app/src/main/java/com/twb/itemrecommender/feature/settings/SettingsActivity.java
+++ app/src/main/java/com/twb/itemrecommender/feature/settings/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -15,24 +16,14 @@ import com.schibstedspain.leku.LocationPickerActivityKt;
 import com.schibstedspain.leku.locale.SearchZoneRect;
 import com.twb.itemrecommender.R;
 import com.twb.itemrecommender.feature.navigation.BaseNavigationActivity;
-
-import timber.log.Timber;
-
-import static com.schibstedspain.leku.LocationPickerActivityKt.LATITUDE;
-import static com.schibstedspain.leku.LocationPickerActivityKt.LONGITUDE;
-import static com.schibstedspain.leku.LocationPickerActivityKt.ZIPCODE;
+import com.twb.itemrecommender.feature.util.Constants;
+import com.twb.itemrecommender.feature.util.SharedPrefsUtils;
 
 public class SettingsActivity extends BaseNavigationActivity {
 
     private static final int MAPS_REQUEST_CODE = 1000;
-
-    private static final double SOUTH_WEST_LAT_COORD = 54.084530;
-    private static final double SOUTH_WEST_LONG_COORD = -8.012903;
-    private static final double NORTH_EAST_LAT_COORD = 55.244539;
-    private static final double NORTH_EAST_LONG_COORD = -5.551857;
-
-    private static final double DEFAULT_CENTER_LATITUDE = (SOUTH_WEST_LAT_COORD + NORTH_EAST_LAT_COORD) / 2;
-    private static final double DEFAULT_CENTER_LONGITUDE = (SOUTH_WEST_LONG_COORD + NORTH_EAST_LONG_COORD) / 2;
+    private TextView locationCoordTextView;
+    private TextView locationAddressTextView;
 
     @Override
     protected int getContentView() {
@@ -45,27 +36,34 @@ public class SettingsActivity extends BaseNavigationActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Settings");
         }
+
+        locationCoordTextView = findViewById(R.id.locationCoordTextView);
+        locationAddressTextView = findViewById(R.id.locationAddressTextView);
+        setLocationUi();
     }
 
     public void onLocationChangeClick(View view) {
-
+        Double latitude = SharedPrefsUtils.getDoublePreference(this, "latitude");
+        if (latitude == null) {
+            latitude = Constants.DEFAULT_CENTER_LATITUDE;
+        }
+        Double longitude = SharedPrefsUtils.getDoublePreference(this, "longitude");
+        if (longitude == null) {
+            longitude = Constants.DEFAULT_CENTER_LONGITUDE;
+        }
         Intent intent = new LocationPickerActivity.Builder()
-                .withLocation(DEFAULT_CENTER_LATITUDE, DEFAULT_CENTER_LONGITUDE)
+                .withLocation(latitude, longitude)
                 .withGeolocApiKey(getString(R.string.google_maps_key))
-                .withSearchZone("es_ES")
-                .withSearchZone(new SearchZoneRect(new LatLng(SOUTH_WEST_LAT_COORD, SOUTH_WEST_LONG_COORD), new LatLng(NORTH_EAST_LAT_COORD, NORTH_EAST_LONG_COORD)))
+                .withSearchZone(new SearchZoneRect(
+                        new LatLng(Constants.SOUTH_WEST_LAT_COORD, Constants.SOUTH_WEST_LONG_COORD),
+                        new LatLng(Constants.NORTH_EAST_LAT_COORD, Constants.NORTH_EAST_LONG_COORD)))
                 .withDefaultLocaleSearchZone()
                 .shouldReturnOkOnBackPressed()
-                .withStreetHidden()
-                .withCityHidden()
-                .withZipCodeHidden()
-                .withSatelliteViewHidden()
                 .withGooglePlacesEnabled()
                 .withGoogleTimeZoneEnabled()
                 .withVoiceSearchHidden()
                 .withUnnamedRoadHidden()
                 .build(this);
-
         startActivityForResult(intent, MAPS_REQUEST_CODE);
     }
 
@@ -82,18 +80,37 @@ public class SettingsActivity extends BaseNavigationActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (resultCode == Activity.RESULT_OK && data != null) {
             if (requestCode == MAPS_REQUEST_CODE) {
-                double latitude = data.getDoubleExtra(LATITUDE, 0.0);
-                Timber.d("LATITUDE %f", latitude);
-                double longitude = data.getDoubleExtra(LONGITUDE, 0.0);
-                Timber.d("LONGITUDE %f", longitude);
+                double latitude = data.getDoubleExtra(LocationPickerActivityKt.LATITUDE, 0d);
+                double longitude = data.getDoubleExtra(LocationPickerActivityKt.LONGITUDE, 0d);
                 String address = data.getStringExtra(LocationPickerActivityKt.LOCATION_ADDRESS);
-                Timber.d("ADDRESS %s", address);
-                String postalcode = data.getStringExtra(ZIPCODE);
-                Timber.d("POSTCODE %s", postalcode);
 
+                SharedPrefsUtils.setDoublePreference(this, "latitude", latitude);
+                SharedPrefsUtils.setDoublePreference(this, "longitude", longitude);
+                SharedPrefsUtils.setStringPreference(this, "address", address);
+
+                setLocationUi();
             }
         } else if (resultCode == Activity.RESULT_CANCELED) {
             Log.d("RESULT****", "CANCELLED");
+        }
+    }
+
+    private void setLocationUi() {
+        Double latitude = SharedPrefsUtils.getDoublePreference(this, "latitude");
+        if (latitude == null) {
+            latitude = Constants.DEFAULT_CENTER_LATITUDE;
+        }
+        Double longitude = SharedPrefsUtils.getDoublePreference(this, "longitude");
+        if (longitude == null) {
+            longitude = Constants.DEFAULT_CENTER_LONGITUDE;
+        }
+        locationCoordTextView.setText(String.format("(%f, %f)", latitude, longitude));
+        String address = SharedPrefsUtils.getStringPreference(this, "address");
+        if (address != null) {
+            locationAddressTextView.setText(address);
+            locationAddressTextView.setVisibility(View.VISIBLE);
+        } else {
+            locationAddressTextView.setVisibility(View.GONE);
         }
     }
 }
---
+++ app/src/main/java/com/twb/itemrecommender/feature/util/Constants.java
@@ -0,0 +1,12 @@
+package com.twb.itemrecommender.feature.util;
+
+public class Constants {
+
+    public static final double SOUTH_WEST_LAT_COORD = 54.084530;
+    public static final double SOUTH_WEST_LONG_COORD = -8.012903;
+    public static final double NORTH_EAST_LAT_COORD = 55.244539;
+    public static final double NORTH_EAST_LONG_COORD = -5.551857;
+
+    public static final double DEFAULT_CENTER_LATITUDE = (SOUTH_WEST_LAT_COORD + NORTH_EAST_LAT_COORD) / 2;
+    public static final double DEFAULT_CENTER_LONGITUDE = (SOUTH_WEST_LONG_COORD + NORTH_EAST_LONG_COORD) / 2;
+}
